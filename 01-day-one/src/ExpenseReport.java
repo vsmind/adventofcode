1208b1052da142d8cff0bf9eb1cc17505b94e5d8
Vitaly Semenov <mtgmind@gmail.com>
2020-12-06 22:49:42 +0100

    Refactoring for day one code

--- 01-day-one/src/ExpenseReport.java
+++ 01-day-one/src/ExpenseReport.java
@@ -8,28 +8,27 @@ import java.util.stream.Stream;
 public class ExpenseReport {
 
     public static void main(String[] args) {
-        String expenseReportFilePath = "day-one/src/input";
-        //String expenseReportFilePath = "src/testInput";
+        String expenseReportFilePath = "01-day-one/src/input";
+        //String expenseReportFilePath = "01-day-one/src/testInput";
 
         try (Stream<String> stream = Files.lines(Paths.get(expenseReportFilePath))){
             List<Integer> expenses = stream.map(Integer::parseInt)
                     .collect(Collectors.toList());
-            List<Integer> expensesCopy = expenses;
-            List<Integer> expensesCopyTwo = expenses;
 
+            starting_loop:
             for (Integer expense: expenses ) {
-                for (Integer expenseCopy: expensesCopy) {
-                    for (Integer expenseCopyTwo: expensesCopyTwo) {
+                for (Integer expenseCopy: expenses) {
+                    for (Integer expenseCopyTwo: expenses) {
                         if (expense + expenseCopy + expenseCopyTwo == 2020) {
                             System.out.println(expense);
                             System.out.println(expenseCopy);
                             System.out.println(expenseCopyTwo);
                             System.out.println(expense * expenseCopy * expenseCopyTwo);
+                            break starting_loop;
                         }
                     }
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
