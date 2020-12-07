f03ea34294ff1066e274732d8add3030b38ab9f3
Vitaly Semenov <mtgmind@gmail.com>
2020-12-07 23:36:11 +0100

    day three refactoring

--- 03-day-three/README.md
+++ 03-day-three/README.md
@@ -58,3 +58,23 @@ The locations you'd check in the above example are marked here with O where ther
 In this example, traversing the map using this slope would cause you to encounter 7 trees.
 
 Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+
+> *Your puzzle answer was **274**.*
+
+# --- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+- Right 1, down 1.
+- Right 3, down 1. (This is the slope you already checked.)
+- Right 5, down 1.
+- Right 7, down 1.
+- Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
+> *Your puzzle answer was **6050183040**.*
--- 03-day-three/src/TobogganTrajectory.java
+++ 03-day-three/src/TobogganTrajectory.java
@@ -10,13 +10,10 @@ public class TobogganTrajectory {
 
     public static void main(String[] args) {
 
-        int tree11 = 0;
-        int tree31 = 0;
-        int tree51 = 0;
-        int tree71 = 0;
-        int tree12 = 0;
+        int tree11, tree31, tree51, tree71, tree12;
+        char[][] map11, map31, map51, map71 ,map12;
 
-        String mapFile = "day-three/src/input";
+        String mapFile = "03-day-three/src/input";
         char[][] map;
 
         try (Stream<String> stream = Files.lines(Paths.get(mapFile))){
@@ -36,54 +33,33 @@ public class TobogganTrajectory {
                 currentLine++;
             }
 
-            char[][] map11;
-            char[][] map31;
-            char[][] map51;
-            char[][] map71;
-            char[][] map12;
             map11 = map.clone();
             map31 = map.clone();
             map51 = map.clone();
             map71 = map.clone();
             map12 = map.clone();
 
-
-            //printMap(mapX, mapY, map);
-            //changePosition(0,0, map);
             tree11 = move(0,0, map11, mapX, mapY, 1, 1);
             tree31 = move(0,0, map31, mapX, mapY, 3, 1);
             tree51 = move(0,0, map51, mapX, mapY, 5, 1);
             tree71 = move(0,0, map71, mapX, mapY, 7, 1);
             tree12 = move(0,0, map12, mapX, mapY, 1, 2);
 
-            //printMap(mapX, mapY, map);
-            System.out.println("Tree: " + tree11);
-            System.out.println("Tree: " + tree31);
-            System.out.println("Tree: " + tree51);
-            System.out.println("Tree: " + tree71);
-            System.out.println("Tree: " + tree12);
-
-            BigInteger totaltrees = BigInteger.valueOf(tree11)
+            BigInteger totalTrees = BigInteger.valueOf(tree11)
                     .multiply(BigInteger.valueOf(tree31))
                     .multiply(BigInteger.valueOf(tree51))
                     .multiply(BigInteger.valueOf(tree71))
                     .multiply(BigInteger.valueOf(tree12));
 
-            printMap(mapX, mapY, map12);
-
-            System.out.println("totaltrees: " + totaltrees);
-
-            System.out.println("All of the Tree: " + totaltrees);
-
+            System.out.println("All of the Tree: " + totalTrees);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    public static void printMap(int maxX, int maxY, char[][] map){
+    // Used in development
+    private static void printMap(int maxX, int maxY, char[][] map){
         for (int i = 0; i < maxY; i++){
-
             for (int j = 0; j < maxX; j++) {
                 System.out.print(map[j][i]);
             }
@@ -91,7 +67,7 @@ public class TobogganTrajectory {
         }
     }
 
-    public static int move(int xPos, int yPos, char[][] map, int maxX, int maxY, int stepX, int stepY) {
+    private static int move(int xPos, int yPos, char[][] map, int maxX, int maxY, int stepX, int stepY) {
         int trees = 0;
         for (int y = 0; y < maxY; y++) {
             if (xPos >= maxX)
@@ -105,10 +81,7 @@ public class TobogganTrajectory {
         return trees;
     }
 
-    public static int changePosition(int xPos, int yPos, char[][] map) {
-        //System.out.println("X: " + xPos);
-        //System.out.println("Y: " + yPos);
-
+    private static int changePosition(int xPos, int yPos, char[][] map) {
         if (map[xPos][yPos] == '#' || map[xPos][yPos] == 'X'){
             map[xPos][yPos] = 'X';
             return 1;
