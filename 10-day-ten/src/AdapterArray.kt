80b935c0e1fb1f33f84b5b3d3181fa82d80b9986
Vitaly Semenov <mtgmind@gmail.com>
2020-12-10 18:26:39 +0100

    day ten part 1

--- 10-day-ten/src/AdapterArray.kt
+++ 10-day-ten/src/AdapterArray.kt
@@ -1,10 +1,40 @@
 import java.io.File
 
 fun main() {
-    val adaptersList = readInput("10-day-ten/src/inputTestSmall")
+    //var adaptersList = readInput("10-day-ten/src/inputTestSmall")
     //val adaptersList = readInput("10-day-ten/src/inputTest")
-    //val adaptersList = readInput("10-day-ten/src/input")
+    val adaptersList = readInput("10-day-ten/src/input")
     println(adaptersList)
+
+    val joltageOne = mutableListOf<Int>()
+    val joltageThree = mutableListOf<Int>()
+
+    adaptersList.maxOrNull()?.plus(3)?.let { adaptersList.add(it) }
+    getJoltDifference(adaptersList, joltageOne, joltageThree)
+
+    findJoltageValue(joltageOne, joltageThree)
 }
 
-fun readInput(filename: String): List<Int> = File(filename).useLines { it.toList() }.map { it.toInt() }
+fun getJoltDifference(adaptersList: List<Int>,
+                      joltageOne: MutableList<Int>,
+                      joltageThree: MutableList<Int>) {
+    var prevJoltage = 0
+
+    adaptersList.sorted().forEach {
+        when (it - prevJoltage) {
+            1 -> joltageOne.add(it)
+            3 -> joltageThree.add(it)
+        }
+        prevJoltage = it
+    }
+
+    println(joltageOne.size)
+    println(joltageThree.size)
+}
+
+fun findJoltageValue(joltageOne: MutableList<Int>,
+                     joltageThree: MutableList<Int>) {
+    println("Number of 1-jolt differences multiplied by the number of 3-jolt differences: " + (joltageOne.size * joltageThree.size))
+}
+
+fun readInput(filename: String): MutableList<Int> = File(filename).useLines { it.toList() }.map { it.toInt() }.toMutableList()
