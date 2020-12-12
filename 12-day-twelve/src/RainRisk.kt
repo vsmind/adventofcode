b7c0258ba5970b2ecb37a92298aa30adc23f54f0
Vitaly Semenov <mtgmind@gmail.com>
2020-12-12 23:48:00 +0100

    day twelve part 2

--- 12-day-twelve/src/RainRisk.kt
+++ 12-day-twelve/src/RainRisk.kt
@@ -8,9 +8,51 @@ fun main() {
 
     val manhattanDistance = findManhattanDistance(navigationInstructions)
     println("Ship manhattan distance: $manhattanDistance")
+
+    println("*********************************************************")
+
+    val manhattanDistanceWaypoint =  findManhattanDistanceWithWaypoint(navigationInstructions)
+    println("Ship manhattan distance waypoint: $manhattanDistanceWaypoint")
 }
 
 data class ShipDirections(val directions: Directions, val navigationUnit: Int)
+
+fun findManhattanDistanceWithWaypoint(shipDirections: List<ShipDirections>) : Int {
+    var shipPositionEastWest = 0
+    var shipPositionNorthSouth = 0
+
+    var waypointEastWest = 10
+    var waypointNorthSouth = 1
+
+    for (instruction in shipDirections) {
+        when(instruction.directions) {
+            Directions.NORTH -> waypointNorthSouth += instruction.navigationUnit
+            Directions.SOUTH -> waypointNorthSouth -= instruction.navigationUnit
+            Directions.EAST -> waypointEastWest += instruction.navigationUnit
+            Directions.WEST -> waypointEastWest -= instruction.navigationUnit
+            Directions.LEFT -> {
+                repeat(instruction.navigationUnit / 90) {
+                    waypointEastWest.let { waypointEastWest = -waypointNorthSouth; waypointNorthSouth = it }
+                }
+            }
+            Directions.RIGHT -> {
+                repeat(instruction.navigationUnit / 90) {
+                    waypointEastWest.let { waypointEastWest = waypointNorthSouth; waypointNorthSouth = -it }
+                }
+            }
+            Directions.FORWARD -> {
+                shipPositionEastWest += waypointEastWest * instruction.navigationUnit
+                shipPositionNorthSouth += waypointNorthSouth * instruction.navigationUnit
+            }
+            else -> println("Instruction ${instruction.directions} not supported")
+        }
+    }
+
+    println("North South Position:  " + shipPositionEastWest.absoluteValue)
+    println("East West Position:    " + shipPositionNorthSouth.absoluteValue)
+
+    return shipPositionEastWest.absoluteValue + shipPositionNorthSouth.absoluteValue
+}
 
 fun findManhattanDistance(shipDirections: List<ShipDirections>) : Int {
     var direction = 90
@@ -33,18 +75,16 @@ fun findManhattanDistance(shipDirections: List<ShipDirections>) : Int {
             }
             Directions.LEFT -> {
                 direction = (360 + direction - instruction.navigationUnit) % 360
-                println(direction)
             }
             Directions.RIGHT -> {
                 direction = (360 + direction + instruction.navigationUnit) % 360
-                println(direction)
             }
-            else -> println("Error")
+            else -> println("Instruction ${instruction.directions} not supported")
         }
     }
 
-    println("northSouthPosition: " + northSouthPosition.absoluteValue)
-    println("eastWestPosition: " + eastWestPosition.absoluteValue)
+    println("North South Position:  " + northSouthPosition.absoluteValue)
+    println("East West Position:    " + eastWestPosition.absoluteValue)
 
     return eastWestPosition.absoluteValue + northSouthPosition.absoluteValue
 }
