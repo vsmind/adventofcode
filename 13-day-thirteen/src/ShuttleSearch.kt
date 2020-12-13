76198eb88ddb2d664633d62b908615cbddabef06
Vitaly Semenov <mtgmind@gmail.com>
2020-12-13 21:33:24 +0100

    day thirteen part 1

--- 13-day-thirteen/src/ShuttleSearch.kt
+++ 13-day-thirteen/src/ShuttleSearch.kt
@@ -1,3 +1,36 @@
-fun main() {
+import java.io.File
+import java.sql.Timestamp
 
+fun main() {
+    //val input = readInput("13-day-thirteen/src/inputTest")
+    val input = readInput("13-day-thirteen/src/input")
+    val earliestTimestamp = input[0].toInt()
+    val busDeparture = input[1].replace("x","")
+            .replace(",", " ")
+            .split(" ")
+            .filter { it != "" }
+            .toTypedArray()
+            .map { it.toInt() }
+
+    findBus(busDeparture, earliestTimestamp)
 }
+
+fun findBus(busDeparture: List<Int>, earliestTimestamp: Int) {
+    val timeList = mutableListOf<Bus>()
+    for (bus in busDeparture) {
+        var time = bus
+        while (time < earliestTimestamp) {
+            time+=bus
+        }
+
+        timeList.add(Bus(bus, time % earliestTimestamp))
+    }
+
+    timeList.sortBy { it.timeList }
+    val bus = timeList.first()
+    println("ID of the earliest bus you can take to the airport multiplied by the number of minutes: " + bus.busNumber * bus.timeList)
+}
+
+fun readInput(filename: String): List<String> = File(filename).useLines { it.toList() }
+
+data class Bus(val busNumber: Int, val timeList: Int)
