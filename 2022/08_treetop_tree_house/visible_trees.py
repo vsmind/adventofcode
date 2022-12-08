7f0cb7e16c42c8edde284902e8c8ce048fe86261
Vitaly Semenov <Vitaly.Semenov@skatteetaten.no>
2022-12-08 14:37:58 +0100

    Day 8 part 2 refactoring needed

--- 2022/08_treetop_tree_house/visible_trees.py
+++ 2022/08_treetop_tree_house/visible_trees.py
@@ -12,6 +12,7 @@ def parse_input(trees):
 
 def find_visible_trees(trees):
     visible_t = 0
+    ss = []
     for i, tree_row in enumerate(trees):
         if i == 0 or i == len(trees) - 1:
             visible_t += len(trees)
@@ -22,6 +23,9 @@ def find_visible_trees(trees):
                 else:
                     if visible(trees, tree, i, j):
                         visible_t += 1
+                    ss.append(scenic_score(trees, tree, i, j))
+    print("scenic score", ss)
+    print("Max scenic score", max(ss))
     return visible_t
 
 
@@ -65,6 +69,71 @@ def left(trees, tree, i, j):
 def right(trees, tree, i, j):
     if max(trees[i][j+1:]) < tree:
         return True
+
+
+def scenic_score(trees, tree, i, j):
+    top = top_score(trees, tree, i, j)
+    bottom = bottom_score(trees, tree, i, j)
+    left = left_score(trees, tree, i, j)
+    right = right_score(trees, tree, i, j)
+    # print(top, left, bottom, right)
+    return top * bottom * left * right
+
+def top_score(trees, tree, i, j):
+    top_trees = []
+    tree_score = 0
+    for k, check in enumerate(trees):
+        if k < i:
+            top_trees.append(check[j])
+    top_trees.reverse()
+    for tr in top_trees:
+        if tr >= tree:
+            tree_score += 1
+            break
+        else:
+            tree_score += 1
+    return tree_score
+
+
+def bottom_score(trees, tree, i, j):
+    bottom_trees = []
+    tree_score = 0
+    for k, check in enumerate(trees):
+        if k > i:
+            bottom_trees.append(check[j])
+    for tr in bottom_trees:
+        if tr >= tree:
+            tree_score += 1
+            break
+        else:
+            tree_score += 1
+    return tree_score
+
+
+def left_score(trees, tree, i, j):
+    tree_score = 0
+    left_trees = trees[i][:j]
+    left_trees.reverse()
+    for tr in left_trees:
+        if tr >= tree:
+            tree_score += 1
+            break
+        else:
+            tree_score += 1
+    return tree_score
+
+
+def right_score(trees, tree, i, j):
+    tree_score = 0
+    right_trees = trees[i][j+1:]
+    # print(right_trees)
+    for tr in right_trees:
+        if tr >= tree:
+            tree_score += 1
+            break
+        else:
+            tree_score += 1
+    return tree_score
 
 
 def visible_trees():
